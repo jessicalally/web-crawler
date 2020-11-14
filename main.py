97daf3a822f71f467df3d1c374b9230b85e7ace8
jml19 <jessica.lally19@imperial.ac.uk>
2020-11-14 14:37:37 +0000

    Refactor and add set to store scraped unique urls

--- main.py
+++ main.py
@@ -3,37 +3,59 @@ import requests
 from bs4 import BeautifulSoup
 from collections import deque
 
+ROOT_URL = r'http(s)?://[^/]+'
+ABSOLUTE_ADDRESS = r'http(s)?://'
+
+unique_urls = set()
+urls_queue = deque()
+
+
+def get_relative_url(url: str, link: str) -> str:
+    if link.startswith('/'):
+        root_url = re.match(ROOT_URL, url).group(0)
+        return root_url + link
+    else:
+        return url + '/' + link
+
+
+def get_url(url: str, link: str):
+    is_absolute_address = re.match(ABSOLUTE_ADDRESS, link)
+
+    if is_absolute_address:
+        urls_queue.append(link)
+    else:
+        # we want to ignore links to other sections of the same webpage as we have already
+        # scraped the urls on the whole of this page
+        if '#' not in link:
+            urls_queue.append(get_relative_url(url, link))
+
+
+def parse_links(url: str, parser: BeautifulSoup):
+    for link in parser.findAll('a'):
+        link = link.get('href')
+
+        if link is not None:
+            get_url(url, link)
+
 
 def scrape_links(base_url: str) -> None:
-    urls = deque()
-    urls.append(base_url)
-    num_links = 1
+    num_urls_scraped = 1
+    urls_queue.append(base_url)
 
-    while num_links < 5:
-        next_url = urls.pop()
-        response = requests.get(next_url)
+    while num_urls_scraped <= 10:
+        next_url = urls_queue.pop()
 
-        if response.status_code == 200:
-            root_url = re.match(r'http(s)?://[^/]+', next_url).group(0)
-            parser = BeautifulSoup(response.content, 'html.parser')
+        # Checks whether url has already been scraped to prevent loops
+        if next_url not in unique_urls:
+            response = requests.get(next_url)
 
-            for link in parser.findAll('a'):
-                link = link.get('href')
-                is_absolute_address = re.match(r'http(s)?://', link)
-
-                if is_absolute_address:
-                    urls.append(link)
-                else:
-                    if '#' not in link:
-                        # we want to ignore links to other sections of the same webpage as we have already
-                        # scraped the urls on the whole of this page
-                        if link.startswith('/'):
-                            urls.append(root_url + link)
-                        else:
-                            urls.append(next_url + '/' + link)
-
-            num_links += 1
-            print(next_url)
+            # Checks if request has succeeded
+            if response.status_code == 200:
+                unique_urls.add(next_url)
+                parser = BeautifulSoup(response.content, 'html.parser')
+                parse_links(next_url, parser)
+                print(next_url)
+                num_urls_scraped += 1
 
 
 def main() -> None:
