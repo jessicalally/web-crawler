caec3b4e4f8529a2606610205253aea2f77ee95c
jml19 <jessica.lally19@imperial.ac.uk>
2020-11-14 17:14:38 +0000

    Implement concurrent crawling

--- main.py
+++ main.py
@@ -1,11 +1,15 @@
 import re
+from concurrent.futures._base import Future
+
 import requests
+import concurrent.futures
 from bs4 import BeautifulSoup
 from collections import deque
 
 URL = r'http(s)?://[^/]+'
 ABSOLUTE_ADDRESS = r'http(s)?://'
-TOTAL_URLS_TO_SCRAPE = 10
+TOTAL_URLS_TO_SCRAPE = 5
+TOTAL_URLS_TO_PROCESS = 100
 SUCCESS_STATUS_CODE = 200
 
 prev_urls = set()
@@ -20,46 +24,84 @@ def get_relative_url(url: str, link: str) -> str:
         return url + '/' + link
 
 
-def get_url(url: str, link: str):
+def get_url(base_url: str, link: str) -> str:
     is_absolute_address = re.match(ABSOLUTE_ADDRESS, link)
 
     if is_absolute_address:
-        urls_queue.append(link)
-    else:
+        return link
+    elif '#' not in link:
         # we want to ignore links to other sections of the same webpage as we have already
         # scraped the urls on the whole of this page
-        if '#' not in link:
-            urls_queue.append(get_relative_url(url, link))
+        return get_relative_url(base_url, link)
 
 
-def parse_links(url: str, parser: BeautifulSoup):
+def parse_links(base_url: str, parser: BeautifulSoup) -> [str]:
+    links = set()
+
     for link in parser.findAll('a'):
-        link = link.get('href')
+        if (link := link.get('href')) is not None:
+            if (url := get_url(base_url, link)) is not None:
+                links.add(url)
 
-        if link is not None:
-            get_url(url, link)
+    return links
+
+
+def task(url: str) -> (bool, [str]):
+    response = requests.get(url)
+
+    # checks if request has succeeded
+    if response.status_code == SUCCESS_STATUS_CODE:
+        parser = BeautifulSoup(response.content, 'html.parser', from_encoding="iso-8859-1")
+        print(url)
+        return True, parse_links(url, parser)
+
+    return False, []
+
+
+def process_futures(futures: {Future, str}):
+    # check status of futures currently working
+    done, not_done = concurrent.futures.wait(
+        futures, None,
+        return_when=concurrent.futures.FIRST_COMPLETED)
+
+    # processes any completed futures
+    for future in done:
+        url = futures[future]
+
+        try:
+            (success, links) = future.result()
+        except Exception as exc:
+            print("The scraped url %s generated an exception: %s" % (url, exc))
+        else:
+            if success:
+                urls_queue.extend(links)
 
 
 def scrape_links(base_url: str) -> None:
-    num_urls_scraped = 1
     urls_queue.append(base_url)
+    processed_urls = 0
 
-    while num_urls_scraped <= TOTAL_URLS_TO_SCRAPE:
-        next_url = urls_queue.pop()
+    with concurrent.futures.ThreadPoolExecutor(max_workers=100) as executor:
+        futures = dict()
 
-        # Checks whether url has already been scraped to prevent loops
-        if next_url in prev_urls:
-            continue
+        while True:
+            # processes any incoming urls
+            while len(urls_queue) != 0:
+                if processed_urls >= TOTAL_URLS_TO_PROCESS:
+                    process_futures(futures)
+                    return
 
-        response = requests.get(next_url)
+                next_url = urls_queue.pop()
 
-        # Checks if request has succeeded
-        if response.status_code == SUCCESS_STATUS_CODE:
-            prev_urls.add(next_url)
-            parser = BeautifulSoup(response.content, 'html.parser')
-            parse_links(next_url, parser)
-            print(next_url)
-            num_urls_scraped += 1
+                # checks whether url has already been scraped to prevent loops
+                if next_url in prev_urls:
+                    continue
+
+                futures[executor.submit(task, next_url)] = next_url
+                processed_urls += 1
+                prev_urls.add(next_url)
+
+            process_futures(futures)
 
 
 def main() -> None:
